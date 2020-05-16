a8518ba6a703c24e137c14af78aa4766d25aa908
SebastianTelles <64764468+SebastianTelles@users.noreply.github.com>
2020-05-16 00:26:52 -0500

    Codigo de Conexion HTTP
    Se probo con un servidor prueba y fue exitoso. Falta agregar el ID.

--- RFID_NodeMcu.ino
+++ RFID_NodeMcu.ino
@@ -1,42 +1,28 @@
-#include <ESP8266WiFi.h> //Librería de NodeMCU
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include <ESP8266WiFi.h>
 
-//Configuración internet//
-const char* ssid = "SSID"; // Rellena con el nombre de tu red WiFi
-const char* password = "Contraseña"; // Rellena con la contraseña de tu red WiFi
- 
-const char* host = "api.tanque.com";
-const char* apiKey = " clave API "; 
+//Put your SSID & Password//
+const char* ssid = "INFINITUM15C4_2.4"; // Rellena con el nombre de tu red WiFi
+const char* password = "LvyPTDBn2L"; // Rellena con la contraseña de tu red WiFi
 
-// Huella digital del certificado del servidor 
-const char * fingerprint = "a9 1f b9 fe 35 b8 38 b9 54 67 e7 34 52 8a 24 d4 17 29 fa 32";
-//----------------------//
+
+ //IP 192.168.1.67   192.168.1.66
+const char* host = "192.168.1.66";
 
 //Variables para lector//
-int tanqueID=0;
-//---------------------//
+String pelota = "12343";
 
-//Serial
+WiFiClient client;
 
 void setup() {
-
-  s.begin(115200);
-  delay(10);
- 
+  Serial.begin(9600);
+  delay(1000);
+  
   // Conectamos a la red WiFi
- 
-  Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
- 
-  /* Configuramos el ESP8266 como cliente WiFi. Si no lo hacemos 
-  se configurará como cliente y punto de acceso al mismo tiempo */
-  WiFi.mode(WIFI_STA); // Modo cliente WiFi
   WiFi.begin(ssid, password);
- 
+
   // Esperamos a que estemos conectados a la red WiFi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -47,85 +33,65 @@ void setup() {
   Serial.println("WiFi connected"); 
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP()); // Mostramos la IP
+
+  //Impresión MAC addres
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
 }
- 
-void loop() {
+
+void loop() 
   
-  tanqueID = obtainingID();
+{
   
-  Serial.print("connecting to ");
-  Serial.println(host);
-
-  /* 
-  // Creamos el cliente
-  WiFiClientSecure client;
-  const int httpPort = 443; // Puerto HTTPS
-  if (!client.connect(host, httpPort)) {
-    // ¿hay algún error al conectar?
-    Serial.println("Ha fallado la conexión");
-    return;
-  }
- 
-  // Verificamos que el certificado coincide con el esperado
-  if(client.verify(fingerprint,host)){
-    Serial.println("El certificado es correcto"); 
-  } else {
-    Serial.println("Error de certificado"); 
-  }
-  */
-
-  // Creamos la URL para la petición
+  if (client.connect(host,3000))     // "184.106.153.149" or api.thingspeak.com
   
-  String url = "/api/";
-  url += apiKey;
-  url += tanqueID;
-  url += ".json";
+  { 
+  String content = pelota;
 
- //Enviamos por bluetooth
-  s.write(123);
-  Serial.print("URL de la petición: https://");
-  Serial.print(host);
-  Serial.print(":");
-  //Serial.print(httpPort);
-  Serial.println(url);
-
-  /*
-  // Enviamos la petición
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                "Host: " + host + "\r\n" +
+   client.print(String("POST /") + " HTTP/1.1\r\n"+
+                "Host: " + host + "\r\n" + 
+                content + "\r\n" +
                 "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
+                
+ // client.print(String("POST /") + " HTTP/1.1\r\n"+
+   //             "Host: " + host + "\r\n" + 
+     //           "Accept: */*\r\n"+
+       //         "Content-Length: " + content.length()+"\r\n" +
+         //       "Content-Type: application/x-www-form-urlencoded"+"\r\n"+
+           //     " " + "\r\n"
+             //   + content);
+                
+    
+  //  client.println("POST / HTTP/1.1");
+   // client.println("Host:" + host);
+   // client.println("Accept: */*");
+  //  client.println("Content-Length: " + content.length());
+   // client.println("Content-Type: application/x-www-form-urlencoded");
+    //client.println();
+    //client.println(content); 
+
+              
+
+              
+    unsigned long timeout = millis();
+
+    
+    while (client.available() == 0) {
+     
     if (millis() - timeout > 5000) {
       Serial.println(">>> Superado el tiempo de espera !");
       client.stop();
       return;
+      }
     }
-  }
 
-  // Consutar la memoria libre
-  // Utilizando HTTPS se necesitan unos 20 kB de memoria libres adicionales
-  Serial.printf("\nMemoria libre en el ESP8266: %d Bytes\n\n",ESP.getFreeHeap());
- 
-  // Leemos la respuesta y la enviamos al monitor serie
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
+    while(client.available()){
+      
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    Serial.println("");
   }
-  Serial.println(); 
-  */
 }
 
-int obtainingID(){
-  Serial.print("El tanqueID es: ");
-  Serial.print(tanqueID);
-  Serial.println();
-  while (tanqueID == 0){
-
-  
-  }
-  Serial.print("El tanqueID es: ");
-  Serial.print(tanqueID);
-  Serial.println();
-  return tanqueID;
-}
